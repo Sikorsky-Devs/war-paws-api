a0fa6fb48850b335f1ab0fa0e5b7bc94947f8478
Bogdan Yarmolka <65012653+thebladehit@users.noreply.github.com>
2025-04-11 21:48:10 +0300

    feat: create contact module (#2)
    * Implement contact service
    
    * Implement contact module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './api/auth/auth.module';
 import { UserModule } from './api/user/user.module';
 import { PetModule } from './api/pet/pet.module';
+import { ContactModule } from './api/contact/contact.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PetModule],
+  imports: [AuthModule, UserModule, PetModule, ContactModule],
 })
 export class AppModule {}
